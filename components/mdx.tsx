313b3331fd53b3f772a59530edd9c0406ff7ff41
Johnie Hjelm <johnie@hjelm.im>
2024-08-31 19:48:25 +0200

    refactor: Update MDX component to include ProsCard and ConsCard components

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -30,7 +30,7 @@ type CustomImageProps = ImageProps & {
 };
 
 const RoundedImage: React.FC<CustomImageProps> = ({ alt, ...props }) => {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+  return <Image alt={alt} className="rounded-lg" {...props} />;
 };
 
 interface CalloutProps {
@@ -79,6 +79,64 @@ const H3: React.FC<HeadingProps> = ({ children, ...props }) => {
   );
 };
 
+type ProsCardProps = {
+  title: string;
+  pros: string[];
+};
+
+function ProsCard({ title, pros }: ProsCardProps) {
+  return (
+    <div className="border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-4 w-full">
+      <span>{`You might use ${title} if...`}</span>
+      <div className="mt-4">
+        {pros.map((pro) => (
+          <div key={pro} className="flex font-medium items-baseline mb-2">
+            <div className="h-4 w-4 mr-2">
+              <svg className="h-4 w-4 text-emerald-500" viewBox="0 0 24 24">
+                <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                  <path d="M22 4L12 14.01l-3-3" />
+                </g>
+              </svg>
+            </div>
+            <span>{pro}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+type ConsCardProps = {
+  title: string;
+  cons: string[];
+};
+
+function ConsCard({ title, cons }: ConsCardProps) {
+  return (
+    <div className="border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-6 w-full">
+      <span>{`You might not use ${title} if...`}</span>
+      <div className="mt-4">
+        {cons.map((con) => (
+          <div key={con} className="flex font-medium items-baseline mb-2">
+            <div className="h-4 w-4 mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-4 w-4 text-red-500"
+              >
+                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+              </svg>
+            </div>
+            <span>{con}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 const components = {
   Image: RoundedImage,
   a: CustomLink,
@@ -86,6 +144,8 @@ const components = {
   h1: H1,
   h2: H2,
   h3: H3,
+  ProsCard,
+  ConsCard,
 };
 
 interface MdxProps {
@@ -95,9 +155,5 @@ interface MdxProps {
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
-  return (
-    <article className="prose prose-quoteless prose-neutral dark:prose-invert max-w-none">
-      <Component components={components as MDXComponents} />
-    </article>
-  );
+  return <Component components={components as MDXComponents} />;
 }
