8d1efead8695713432f0da244ffb37758ccfe5d6
Johnie Hjelm <johnie@hjelm.im>
2024-11-12 19:58:51 +0100

    feat: enhance LatestWriting component to support view counts with a new Views function and optimized layout for readability

--- components/latestWriting.tsx
+++ components/latestWriting.tsx
@@ -1,8 +1,17 @@
 import { type Post } from 'content-collections';
 import Link from 'next/link';
 import { format } from 'date-fns';
+import { Suspense } from 'react';
+import { getViewsCount } from '@/lib/actions';
+import ViewCounter from '@/components/ViewCounter';
 
-export const LatestWriting = ({ items, amount }: { items: Post[]; amount: number }) => {
+export const LatestWriting = ({
+  items,
+  amount,
+}: {
+  items: Post[];
+  amount: number;
+}) => {
   if (!items) {
     return null;
   }
@@ -23,11 +32,17 @@ export const LatestWriting = ({ items, amount }: { items: Post[]; amount: number
             href={`/writing/${post.slug}`}
           >
             <div className="w-full flex flex-col">
-              <p className="text-neutral-700 dark:text-neutral-300 tracking-tight text-sm md:text-base">{post.title}</p>
+              <p className="text-neutral-700 dark:text-neutral-300 tracking-tight text-sm md:text-base">
+                {post.title}
+              </p>
               <div className="flex items-center text-sm text-neutral-500 dark:text-neutral-500">
                 <p>{format(new Date(post.publishedAt), 'dd MMMM, yyyy')}</p>
                 <span className="mx-4 ">•</span>
                 <p>{post.readingTime}</p>
+                <span className="mx-4 ">•</span>
+                <Suspense fallback={null}>
+                  <Views slug={post.slug} />
+                </Suspense>
               </div>
             </div>
           </Link>
@@ -35,3 +50,8 @@ export const LatestWriting = ({ items, amount }: { items: Post[]; amount: number
     </div>
   );
 };
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+  return <ViewCounter slug={slug} allViews={views} />;
+}
