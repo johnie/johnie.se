69df5216b88654e683526d7eda0f56ed9e3698e9
Johnie Hjelm <johnie.hjelm@schibsted.com>
2025-11-19 19:54:25 +0100

    feat: update footer component to include Spotify display

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,5 @@
+import { Spotify } from "@/components/spotify";
+
 const GetInTouch = ({ show }: { show: boolean }) => {
   if (!show) {
     return null;
@@ -5,8 +7,8 @@ const GetInTouch = ({ show }: { show: boolean }) => {
 
   return (
     <div className="mb-8 flex items-center text-neutral-400 text-sm">
-      <div className="absolute mr-2 inline-block h-[8px] w-[8px] animate-ping rounded-full bg-green-400" />
-      <div className="mr-2 inline-block h-[8px] w-[8px] rounded-full bg-green-400" />
+      <div className="absolute mr-2 inline-block h-2 w-2 animate-ping rounded-full bg-green-400" />
+      <div className="mr-2 inline-block h-2 w-2 rounded-full bg-green-400" />
       Actively looking for new projects.{" "}
       <a className="border-neutral-400 border-b" href="mailto:johnie@hjelm.im">
         Get in touch!
@@ -18,6 +20,7 @@ const GetInTouch = ({ show }: { show: boolean }) => {
 export const Footer = () => (
   <footer className="my-10">
     <GetInTouch show={false} />
+    <Spotify />
     <h3 className="mb-2 text-neutral-400 text-sm">
       © {new Date().getFullYear().toString()} Johnie Hjelm.
     </h3>
