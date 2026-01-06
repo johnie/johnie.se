7f0f93ab6409e8f39157c65c13bc327154065fa9
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-01-06 13:24:17 +0100

    fix: standardize icon sizes for consistency in AboutActions component

--- components/about-actions.tsx
+++ components/about-actions.tsx
@@ -12,13 +12,13 @@ const downloadHeadshot = () => {
 };
 
 export const AboutActions = ({ bio }: { bio: string }) => (
-  <div className="md:-ml-4 flex items-center justify-between md:justify-start">
+  <div className="flex items-center justify-between md:-ml-4 md:justify-start">
     <button
       className="ease flex items-center rounded-xl border-none px-4 py-2 text-neutral-500 text-sm transition-colors hover:bg-neutral-50 md:text-base dark:text-neutral-400 dark:hover:bg-neutral-900"
       onClick={() => copyBio(bio)}
       type="button"
     >
-      <LucideClipboardCopy className="mr-2 h-[18px] w-[18px] text-current" />
+      <LucideClipboardCopy className="mr-2 h-4.5 w-4.5 text-current" />
       Copy bio
     </button>
     <span className="mx-0 md:mx-4">•</span>
@@ -28,7 +28,7 @@ export const AboutActions = ({ bio }: { bio: string }) => (
       href="/images/johnie-omni.jpg"
       onClick={downloadHeadshot}
     >
-      <LucideFileImage className="mr-2 h-[18px] w-[18px] text-current" />
+      <LucideFileImage className="mr-2 h-4.5 w-4.5 text-current" />
       Download headshot
     </a>
   </div>
