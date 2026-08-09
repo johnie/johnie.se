b84f883569d6175e91800564429902a1d5c0c23d
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-08-09 14:44:10 +0200

    chore(config): update dependencies, next config, env schema, biome, and ts target

--- lib/env.ts
+++ lib/env.ts
@@ -1,9 +1,10 @@
-// src/env.mjs
+// lib/env.ts
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
   runtimeEnv: {
+    EDGE_CONFIG: process.env.EDGE_CONFIG,
     SPOTIFY_API_CLIENT_ID: process.env.SPOTIFY_API_CLIENT_ID,
     SPOTIFY_API_CLIENT_SECRET: process.env.SPOTIFY_API_CLIENT_SECRET,
     SPOTIFY_API_REFRESH_TOKEN: process.env.SPOTIFY_API_REFRESH_TOKEN,
@@ -11,6 +12,7 @@ export const env = createEnv({
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
   },
   server: {
+    EDGE_CONFIG: z.string().optional(),
     SPOTIFY_API_CLIENT_ID: z.string().min(1),
     SPOTIFY_API_CLIENT_SECRET: z.string().min(1),
     SPOTIFY_API_REFRESH_TOKEN: z.string().min(1),
