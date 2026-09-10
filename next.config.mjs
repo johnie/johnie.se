271e08005e15b8b7e6e5d13970f87d382595d085
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-09-10 13:09:04 +0200

    fix(config): define security headers before use

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,47 @@
 import { withContentCollections } from "@content-collections/next";
 import { get } from "@vercel/edge-config";
 
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live pulse.hjelm.cloud;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://i.scdn.co https://media0.giphy.com https://johnie.se;
+    media-src 'none';
+    connect-src 'self' https://pulse.hjelm.cloud https://api.spotify.com https://accounts.spotify.com https://vitals.vercel-insights.com;
+    font-src 'self' data:;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replaceAll("\n", ""),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   headers() {
@@ -50,46 +91,5 @@ const nextConfig = {
   },
   typedRoutes: true,
 };
-
-const ContentSecurityPolicy = `
-    default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live pulse.hjelm.cloud;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://i.scdn.co https://media0.giphy.com https://johnie.se;
-    media-src 'none';
-    connect-src 'self' https://pulse.hjelm.cloud https://api.spotify.com https://accounts.spotify.com https://vitals.vercel-insights.com;
-    font-src 'self' data:;
-`;
-
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replaceAll("\n", ""),
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
-  },
-];
 
 export default withContentCollections(nextConfig);
